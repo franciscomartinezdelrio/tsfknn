7bfecf64a5badd855ec3965b22ed1f521c1e6625
franciscomartinezdelrio <fmartin@ujaen.es>
2017-12-13 14:13:55 +0100

    Documentation added and change in formal parameter name

--- R/ploting.R
+++ R/ploting.R
@@ -1,16 +1,30 @@
-#' @export
 #' @importFrom ggplot2 autoplot
+NULL
+
+#' Create a ggplot object from a knnForecast object.
+#'
+#' It uses a knnForecast object to create a ggplot object that plots a time
+#' series and its forecast using a KNN model.
+#'
+#' @param forecast The knnForecast object.
+#'
+#' @param highlight A boolean value indicating whether the data points
+#'                  should be highlighted using points.
+#'
+#' @return The ggplot object representing a graph with the forecast.
+#'
 #' @examples
 #' pred <- knn_forecasting(USAccDeaths, h = 12, lags = 1:12, k = 2)
 #' library(ggplot2)
 #' autoplot(pred)
-#' autoplot(pred, highlight = T)
-autoplot.knnForecast <- function(model, highlight = FALSE) {
+#' autoplot(pred, highlight = TRUE)
+#' @export
+autoplot.knnForecast <- function(forecast, highlight = FALSE) {
   data <- data.frame(
-    tsx   = as.vector(time(model$timeS)),
-    tsy   = as.vector(model$timeS),
-    predx = as.vector(time(model$pred)),
-    predy = as.vector(model$pred)
+    tsx   = as.vector(time(forecast$timeS)),
+    tsy   = as.vector(forecast$timeS),
+    predx = as.vector(time(forecast$pred)),
+    predy = as.vector(forecast$pred)
   )
   p <- ggplot2::ggplot(data) +
     ggplot2::geom_line(ggplot2::aes(x = tsx, y = tsy, colour = "Original")) +
