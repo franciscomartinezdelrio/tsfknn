59f3cff82e91d70e5ce0b5c91f1654d1bfc4d380
franciscomartinezdelrio <fmartin@ujaen.es>
2017-12-14 09:49:02 +0100

    Change formal parameter h for nt and the way the function returns the knnModel

--- R/knn.R
+++ R/knn.R
@@ -6,27 +6,27 @@
 #' @param timeS The time series.
 #' @param lags An integer vector with the lags used as feature vector in
 #'             increasing order.
-#' @param h The number of targets (amount of horizons to be forecast).
+#' @param nt The number of targets.
 #'
 #' @return A list with two fields: 1) a matrix with the features of the
 #'         examples and 2) a matrix with the targets of the examples
 #' @examples
 #' build_examples(ts(1:5), lags = 1:2)
-#' build_examples(ts(1:5), lags = 1:2, h = 2)
-build_examples <- function(timeS, lags, h = 1) {
+#' build_examples(ts(1:5), lags = 1:2, nt = 2)
+build_examples <- function(timeS, lags, nt = 1) {
   MAXLAG <- tail(lags, 1)
   NCOL = length(lags)
-  NROW = length(timeS) - MAXLAG - h + 1
+  NROW = length(timeS) - MAXLAG - nt + 1
   patterns <- matrix(data = 0, nrow = NROW, ncol = NCOL)
-  targets  <- matrix(data = 0, nrow = NROW, ncol = h)
+  targets  <- matrix(data = 0, nrow = NROW, ncol = nt)
   row <- 1
-  for (ind in seq(MAXLAG + h, length(timeS))) {
-    patterns[row, ] <- timeS[ind - h + 1 - lags]
-    targets[row, ] <- timeS[(ind - h + 1):ind]
+  for (ind in seq(MAXLAG + nt, length(timeS))) {
+    patterns[row, ] <- timeS[ind - nt + 1 - lags]
+    targets[row, ] <- timeS[(ind - nt + 1):ind]
     row <- row + 1
   }
   colnames(patterns) <- paste0("Lag", lags)
-  colnames(targets)  <- paste0("H", 1:h)
+  colnames(targets)  <- paste0("H", 1:nt)
   return(list(patterns = patterns, targets = targets))
 }
 
@@ -38,18 +38,25 @@ build_examples <- function(timeS, lags, h = 1) {
 #' @param lags An integer vector with the lags used as feature vector in
 #'             increasing order.
 #' @param k The k parameter.
-#' @param h The number of targets (amount of horizons to be forecast).
+#' @param nt The number of targets (amount of horizons to be forecast).
 #' @return An object of type knnModel.
 #'
 #' @export
-knn_model <- function(timeS, lags, k, h = 1) {
+knn_model <- function(timeS, lags, k, nt = 1) {
   stopifnot(lags[1] >= 1)
   MAXLAG <- tail(lags, 1)
-  if (MAXLAG + h > length(timeS)) stop("Impossible to create one example")
-  examples <- build_examples(timeS, lags, h)
+  if (MAXLAG + nt > length(timeS)) stop("Impossible to create one example")
+  examples <- build_examples(timeS, lags, nt)
   if (k > nrow(examples$patterns)) stop("k > number of examples")
-  result <- list(ts = timeS, lags = lags, examples = examples, k = k)
-  structure(result, class = "knnModel")
+  structure(
+    list(
+      ts = timeS,
+      lags = lags,
+      examples = examples,
+      k = k
+    ),
+    class = "knnModel"
+  )
 }
 
 #' Change the k parameter of a KNN model.
@@ -73,7 +80,7 @@ knn_model <- function(timeS, lags, k, h = 1) {
 #' @param example The features of the example whose target is to be predicted.
 #'
 #' @export
-#' @example
+#' @examples
 #' model <- knn_model(ts(c(2, 3, 1, 5, 4, 0, 7, 1, 2)), lags = 1:2, k = 2)
 #' regression(model, c(2, 1))
 regression <- function(model, example) {
