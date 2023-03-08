d06e074489c90cf049b00c1b4a6cea7d36b45f11
franciscomartinezdelrio <fmartin@ujaen.es>
2023-03-08 10:18:51 +0100

    GitHub actions

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<!-- badges: start -->
+[![R-CMD-check](https://github.com/franciscomartinezdelrio/tsfknn/actions/workflows/R-CMD-check.yaml/badge.svg)](https://github.com/franciscomartinezdelrio/tsfknn/actions/workflows/R-CMD-check.yaml)
+<!-- badges: end -->
 
 <!-- README.md is generated from README.Rmd. Please edit that file -->
 
--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // first_n
 List first_n(NumericMatrix m, NumericVector v, int k);
 RcppExport SEXP _tsfknn_first_n(SEXP mSEXP, SEXP vSEXP, SEXP kSEXP) {
